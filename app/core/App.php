18aadac9b013ae893174c967ed972f7217ff3839
mahmoodahmad100 <mahmood073@gmail.com>
2017-11-08 09:07:03 +0200

    get all params in the url

--- app/core/App.php
+++ app/core/App.php
@@ -8,14 +8,14 @@ class App
 
 	public function __construct()
 	{
-		$this->parseUrl();
+		$url = $this->parseUrl();
 	}
 
 	public function parseUrl()
 	{
 		if(isset($_GET['url']))
 		{
-			echo $_GET['url'];
+			return $url = explode('/',filter_var(rtrim($_GET['url'],'/'),FILTER_SANITIZE_URL));
 		}
 	}
 }
