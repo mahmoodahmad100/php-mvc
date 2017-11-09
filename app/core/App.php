27ce82d3939fc04db542457d444791424c1e9be6
mahmoodahmad100 <mahmood073@gmail.com>
2017-11-09 21:38:59 +0200

    setting up the controller

--- app/core/App.php
+++ app/core/App.php
@@ -9,6 +9,17 @@ class App
 	public function __construct()
 	{
 		$url = $this->parseUrl();
+
+		if(file_exists('../app/controllers/'.$url[0].'.php'))
+		{
+			$this->controller = $url[0];
+			unset($url[0]);
+		}
+
+		require_once '../app/controllers/'.$this->controller.'.php';
+		$this->controller = new $this->controller;
+
+		var_dump($this->controller);
 	}
 
 	public function parseUrl()
